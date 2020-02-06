857e6a4ee0ba48102c7e4955cfe6d055acaef715
ray <12276029+tesoph@users.noreply.github.com>
2020-02-06 19:49:46 +0000

    Add catch disambiguationerror

--- app/wiki.py
+++ app/wiki.py
@@ -1,5 +1,5 @@
 import wikipedia
-
+from wikipedia import DisambiguationError
 '''
 
 The code that caused this warning is on line 389 of the file 
@@ -22,10 +22,35 @@ headings look like
 #s = wikipedia.summary('Github')
 #s= g.content
 
+#https://www.programcreek.com/python/example/93332/wikipedia.DisambiguationError
+'''
+def get_wiki_page(search_string, summary=False, content=False):
+    #Returns results from searching for search_string with wikipedia wrapper library. 
+       Note: Makes a web request
+    try:
+        page = wikipedia.page(search_string)
+        page_data = {"url":page.url,
+                     "title":page.title}
+        if content:
+            page_data["content"] = page.content # Full text content of page.
+        if summary:
+            page_data["summary"] = page.summary # Summary section only.
+    
+    except DisambiguationError as e:
+        return get_wiki_page(e.options[0]) #naively choose first option
+    except Exception as e:
+        return None
+    
+    return page_data '''
 
 def get_content(term):
-    page = wikipedia.page(term)
-    s= page.content
+    try:
+        page = wikipedia.page(term)
+        s= page.content
+    except DisambiguationError as e:
+        return get_content(e.options[0])
+    except Exception as e:
+        return None
     return s
 
     
