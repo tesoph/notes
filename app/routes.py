f3adefdd6f75ccbdce1960d0623c5d08f4037c9f
ray <12276029+tesoph@users.noreply.github.com>
2020-02-22 17:41:15 +0000

    Add skincare route

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Flask, flash, jsonify, redirect, render_template, request, session, url_for
+from flask import Flask, flash, jsonify, redirect, render_template, request, session, url_for, json
 from app import app, db
 # from app.forms import LoginForm
 from flask_pymongo import PyMongo
@@ -76,6 +76,30 @@ def login_required(f):
             return redirect("/login")
         return f(*args, **kwargs)
     return decorated_function
+
+
+
+@app.route('/skincare', methods=["GET", "POST"])
+def skincare():
+    #GET /product?q=rose+water
+    #https://skincare-api.herokuapp.com/product?q=rose&limit=25&page=1
+    '''
+    filename = os.path.join(app, 'data', 'products.json')
+    with open(filename) as f:
+             d= json.load(f)
+             '''
+    #https://stackoverflow.com/questions/21133976/flask-load-local-json
+    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+    json_url = os.path.join(SITE_ROOT, "data", "products.json")
+    #data = json.load(open(json_url))
+    with open(json_url, "r") as read_file:
+           data = json.load(read_file)
+    print(type(data))
+    #print(data[:2])
+    for c in data[:5]:
+        print(c['brand'])
+
+    return render_template('skincare.html',data=data)
 
 
 @app.route('/')
