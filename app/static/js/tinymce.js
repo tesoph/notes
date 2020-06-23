e5e215a57d346cb4203aeea9b8705f567e457d9b
ray <12276029+tesoph@users.noreply.github.com>
2020-06-23 20:59:23 +0100

    Update tinymce settings, minor styling

--- app/static/js/tinymce.js
+++ app/static/js/tinymce.js
@@ -18,21 +18,20 @@ tinymce.init({
     body_class: 'note_class',
     body_id: 'my_id',
     plugins: [
-        'advlist autolink link image lists charmap print preview hr anchor pagebreak save spellchecker',
+        'advlist autolink link lists charmap print preview hr anchor pagebreak save spellchecker',
         'searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking',
         'table emoticons template paste help',
     ],
     toolbar: 'save | undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | ' +
-        'bullist numlist outdent indent | link image | print preview media fullpage | ' +
+        'bullist numlist outdent indent | link | print preview fullpage | ' +
         'forecolor backcolor emoticons | help',
     menu: {
         file: { title: 'File', items: 'save | restoredraft | preview | print ' },
         edit: { title: 'Edit', items: 'save |undo redo | cut copy paste | selectall | searchreplace' },
         view: { title: 'View', items: 'code | visualaid visualchars visualblocks | spellchecker | preview fullscreen' },
-        insert: { title: 'Insert', items: 'image link media template codesample inserttable | charmap emoticons hr | pagebreak nonbreaking anchor toc | insertdatetime' },
+        insert: { title: 'Insert', items: 'link | emoticons hr | pagebreak nonbreaking toc | insertdatetime' },
         format: { title: 'Format', items: 'bold italic underline strikethrough superscript subscript codeformat | formats blockformats fontformats fontsizes align | forecolor backcolor | removeformat' },
         tools: { title: 'Tools', items: 'spellchecker spellcheckerlanguage | code wordcount' },
-        table: { title: 'Table', items: 'inserttable | cell row column | tableprops deletetable' },
         help: { title: 'Help', items: 'help' }
     },
     content_css: 'css/content.css'
--- notes.py
+++ notes.py
@@ -1,9 +1,7 @@
 import os
 from app import app, db
 # models
-notes = db.notes
-categories = db.categories
-users = db.users
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
