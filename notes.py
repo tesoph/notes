748bac5c315c8c8d4d6aa39f80d7029a9c8703e6
ray <12276029+tesoph@users.noreply.github.com>
2020-07-26 21:02:56 +0100

    update notes.py

--- notes.py
+++ notes.py
@@ -2,8 +2,8 @@ import os
 from app import app, db
 # models
 #import os from pml import app port = int(os.environ.get('PORT', 5000)) app.run(host='0.0.0.0', port=port)
-'''
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
-            debug=True)'''
+            debug=True)
